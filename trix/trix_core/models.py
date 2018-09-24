81b687c6cd0acfbd7b17b23964316ffdfad08174
Jonas Sandbekk <shaitan25@gmail.com>
2018-09-24 13:30:42 +0200

    Fixed flake8 F401, unused import

--- trix/trix_core/models.py
+++ trix/trix_core/models.py
@@ -1,7 +1,6 @@
 import re
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
-from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
