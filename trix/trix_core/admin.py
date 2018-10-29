ed92c80e644351fed3beccd353ca709dcc950e9d
Jonas Sandbekk <shaitan25@gmail.com>
2018-10-29 13:55:22 +0100

    Add forms to be able to create users with password.

--- trix/trix_core/admin.py
+++ trix/trix_core/admin.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.db.models import Count, Q
 from django.utils.translation import ugettext_lazy as _
 
@@ -21,18 +22,50 @@ def unset_administrators(modeladmin, request, queryset):
 unset_administrators.short_description = _("Remove admin access from the selected users")
 
 
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = coremodels.User
+        fields = ('email', 'is_admin')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
 class UserAdmin(admin.ModelAdmin):
+    form = UserCreationForm
     list_display = [
         'email',
         'is_admin',
         'last_login',
     ]
-    search_fields = ['email']
     list_filter = [
         'is_admin',
         'last_login',
     ]
-    fields = ['email', 'is_admin']
+    fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2',
+                           'is_active', 'is_admin', 'last_login')}),
+    )
+    search_fields = ['email']
+    ordering = ['email']
     readonly_fields = ['last_login']
     actions = [set_administrators, unset_administrators]
 
